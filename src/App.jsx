ef7e97eb9bf45eb01c057873d995b8fe69cd5c39
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2024-11-25 11:53:10 +0200

    Remove unnecessary links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import UserCardList from './components/UserCardList';
 
 function App() {
-  return <UserCardList/>
+  return <UserCardList />;
 }
 
 export default App;
