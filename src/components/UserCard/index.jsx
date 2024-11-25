9dde76d39c9673664cc9172a9db9616b2b271219
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2024-11-25 11:37:05 +0200

    Add userCard

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import './UserCard.css';
 
-function UserCard({ id, name, gender }) {
-  const cardClass = gender
-    ? gender === 'male'
-      ? 'user-card male'
-      : 'user-card female'
-    : 'user-card';
+class UserCard extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <>
+    this.state = {
+      isFriend: false,
+    };
+  }
+
+  toggleFriendStatus = () => {
+    this.setState({
+      isFriend: !this.state.isFriend,
+    });
+  };
+
+  render() {
+    const { id, name, gender } = this.props;
+    const { isFriend } = this.state;
+
+    const cardClass = gender
+      ? gender === 'male'
+        ? 'user-card male'
+        : 'user-card female'
+      : 'user-card';
+
+    return (
       <article className={cardClass}>
-        <h2 className="user-card-heading">Name: {name}</h2>
+        <h2 className="user-card-heading">{name}</h2>
         <p className="user-card-id">Id: {id}</p>
-        <p className="user-card-gender"> Gender: {gender}</p>
+        <p className="user-card-gender">Gender: {gender || 'Not Specified'}</p>
+        <button className="btn" onClick={this.toggleFriendStatus}>
+          {isFriend ? 'Remove Friend' : 'Add Friend'}
+        </button>
       </article>
-    </>
-  );
+    );
+  }
 }
 
 export default UserCard;
