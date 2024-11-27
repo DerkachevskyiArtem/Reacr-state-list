56b2e4d594b137ae108ebbb59680c1d3e859f22a
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2024-11-27 21:36:50 +0200

    Add click buttons

--- src/components/Clicker/Clicker.jsx
+++ src/components/Clicker/Clicker.jsx
@@ -8,10 +8,6 @@ class Clicker extends React.Component {
     intervalId: null,
   };
 
-  startAutoClick = () => {
-    if(this.state.isAutoClicking) return
-  }
-
   handleIncrement = () => {
     const { counter } = this.state;
 
@@ -20,12 +16,57 @@ class Clicker extends React.Component {
     });
   };
 
+  changeAutoClickInterval = (newInterval) => {
+    clearInterval(this.state.intervalId);
+
+    const intervalId = setInterval(() => {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+    }, newInterval);
+
+    this.setState({
+      autoClickInterval: newInterval,
+      intervalId: intervalId,
+    });
+  };
+
+  stopAutoClick = () => {
+    if (this.state.intervalId) {
+      clearInterval(this.state.intervalId);
+
+      this.setState({
+        isAutoClicking: false,
+        intervalId: null,
+      });
+    }
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
   render() {
+    const { counter, isAutoClicking } = this.state;
+
     return (
       <article>
         <h2>Click Counter</h2>
-        <p>{this.state.counter}</p>
-        <button onClick={this.handleIncrement}>Click me</button>
+        <p>Clicks: {counter}</p>
+        <button onClick={this.handleIncrement}>Click</button>
+        <button onClick={() => this.changeAutoClickInterval(1000)}>
+          1 Second
+        </button>
+        <button onClick={() => this.changeAutoClickInterval(2000)}>
+          2 second
+        </button>
+        <button onClick={() => this.changeAutoClickInterval(5000)}>
+          5 second
+        </button>
+        <button onClick={() => this.changeAutoClickInterval(10000)}>
+          10 second
+        </button>
+        <button onClick={this.stopAutoClick}>Stop AutoClick</button>
       </article>
     );
   }
